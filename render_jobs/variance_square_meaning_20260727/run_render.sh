5da486cd509cc86d0752c8d00f2107b3fc620fc7
juanperez238421-cpu <juanperez238421@gmail.com>
2026-07-26 21:46:19 -0500

    Fix checksum packaging for variance square render

--- render_jobs/variance_square_meaning_20260727/run_render.sh
+++ render_jobs/variance_square_meaning_20260727/run_render.sh
@@ -117,7 +117,7 @@ EOF
 
 (
   cd delivery
-  sha256sum * control_frames/*.png > SHA256SUMS.txt
+  find . -type f ! -name SHA256SUMS.txt -print0 | sort -z | xargs -0 sha256sum > SHA256SUMS.txt
 )
 
 ls -lh delivery
